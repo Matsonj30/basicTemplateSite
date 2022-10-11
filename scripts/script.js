021c757dcadf9b735892fa3bbfebbadb5b3ac509
Matsonj30 <matsonj2013@gmail.com>
2022-10-10 21:13:10 -0600

    hamburger menu complete, maybe want more effects

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,23 @@
 url = window.location.href
 
 console.log(url)
+
+function showMenu(){
+    var currentMenuState = document.getElementById("burgerMenu");
+    if(currentMenuState.style.display == "block"){
+        currentMenuState.style.display = "none";
+    }
+    else{//it must be none
+        currentMenuState.style.display ="block"
+    }
+    
+}
+window.addEventListener("resize", function(resizeWindow){
+    var currentMenuState = document.getElementById("burgerMenu");
+    if(document.body.clientWidth >= 890){
+        currentMenuState.style.display ="flex"
+    }
+    else{
+        currentMenuState.style.display ="none"
+    }
+})
