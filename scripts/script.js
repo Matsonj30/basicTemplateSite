d87842fffc889d0eb1cb01205e1412c36506c707
Matsonj30 <matsonj2013@gmail.com>
2022-10-20 18:36:54 -0600

    started gallery slideshow

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,17 @@
 
 
 
-function showPage(){
+function showPage(){ //for mainMenu Section
     url = window.location.href;
     console.log(url);
     switch(url){
         case "http://192.168.4.22:5500/templates/home.html":
             var currentPageTab = document.getElementById("homePage");
-            currentPageTab.style.borderBottom = "3px solid #74A57F"
+            currentPageTab.style.borderBottom = "3px solid #025b6d"
             break
         case "http://192.168.4.22:5500/templates/gallery.html":
             var currentPageTab = document.getElementById("galleryPage");
-            currentPageTab.style.borderBottom = "3px solid #74A57F"
+            currentPageTab.style.borderBottom = "3px solid #025b6d"
             break
     }
   {
@@ -20,7 +20,7 @@ function showPage(){
     }
 }
 
-function showMenu(){
+function showMenu(){ //for mainMenu section
     var currentMenuState = document.getElementById("burgerMenu");
     if(currentMenuState.style.display == "block"){
         currentMenuState.style.display = "none";
@@ -39,5 +39,13 @@ window.addEventListener("resize", function(resizeWindow){
         currentMenuState.style.display ="none"
     }
 })
+
+function changePicture(pictureNumber){
+    var selectedImage = document.getElementById("img"+pictureNumber)
+    selectedImage.style.display = "block"
+}
+
+
+
 
 showPage()
